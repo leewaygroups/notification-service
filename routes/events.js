2d3e3533fadebd1f8ab2b0222fb7937a0f0a6a5f
leewaygroups <leewaygroups@gmail.com>
2016-05-02 06:19:58 +0700

    routes

--- routes/events.js
+++ routes/events.js
@@ -11,54 +11,67 @@ var eventTypes = require('../lib/eventTypes');
 "eventSource": ['App.CustomerService']
 }
 ********************************************/
-
-exports.registerEvent = function(req, res) {
-  var event = req.body;
-  return eventManager.register(event);
-}
-
-exports.updateEvent = function(req, res) {
-  var id = req.params.id;
-  var event = req.body;
-  return eventManager.update(event, id);
-}
- 
-exports.deleteEvent = function(req, res) {
-  var id = req.params.id;
-
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('events', function(err, collection) {
-      collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-        res.send(req.body);
-        db.close();
-      });
+exports.registerEvent = function (req, res) {
+    var event = req.body;
+    eventManager.register(event).then(function(result){
+        res.send(result);
     });
-  });
-}
+};
 
-exports.findById = function(req, res) {
-  var id = req.params.id;
-
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('events', function(err, collection) {
-      collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-        res.send(item);
-        db.close();
-      });
+exports.updateEvent = function (req, res) {
+    var id = req.params.id;
+    var event = req.body;
+    eventManager.update(event, id).then(function(result){
+        res.send(result);
     });
-  });
-}
+};
 
- 
-exports.findAll = function(req, res) {
-
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('events', function(er, collection) {
-      collection.find().toArray(function(err, items) {
-        res.send(items);
-        db.close();
-      });
+exports.deleteEvent = function (req, res) {
+    var id = req.params.id;
+    eventManager.delete(id).then(function(result){
+        res.send(result);
     });
-  });
+};
+
+exports.findById = function (req, res) {
+    var id = req.params.id;
+    eventManager.findById(id).then(function(result){
+        res.send(result);
+    });
+};
+
+exports.findAll = function (req, res) {
+    eventManager.findAll().then(function(result){
+        res.send(result);
+    });
+};
+
+exports.findByName = function (req, res) {
+    var name = req.params.name;
+    eventManager.findByName(name).then(function(result){
+        res.send(result);
+    });
+};
+
+exports.eventSubscriptions = function(req, res){
+    var name = req.params.name;
+    eventManager.subsriptions(name).then(function(result){
+        res.send(result);
+    });
 }
- 
+
+exports.subscribe = function (req, res) {
+    var name = req.params.name;
+    var subcr = req.body.subcr;
+    eventManager.subscribe(name, subcr).then(function(result){
+        res.send(result);
+    });
+};
+
+exports.unsubscribe = function(req, res){
+    var name = req.params.name;
+    var subcr = req.body.subcr;
+    eventManager.subscribe(name, subcr).then(function(result){
+        res.send(result);
+    });
+};
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var event = require('./routes/events');
-var sub = require('./routes/subscriptions');
 var signal = require('./routes/signals');
 var signallog = require('./routes/signallog');
 
@@ -10,24 +9,23 @@ var port = (process.env.VCAP_APP_PORT || 3000);
 var app = express();
  
 app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.logger('dev')); 
     app.use(express.bodyParser());
 });
  
 console.log ('registering event routes with express');
 app.get('/events', event.findAll);
 app.get('/events/:id', event.findById);
-app.post('/events', event.addEvent);
+app.get('/events/:name', event.findByName);
+app.post('/events', event.registerEvent);
 app.put('/events/:id', event.updateEvent);
 app.delete('/events/:id', event.deleteEvent);
- 
-console.log ('registering subscription routes with express');
-app.get('/subscriptions', sub.findAll);
-app.get('/subscriptions/:id', sub.findById);
-app.post('/subscriptions', sub.addSubscription);
-app.put('/subscriptions/:id', sub.updateSubscription);
-app.delete('/subscriptions/:id', sub.deleteSubscription);
 
+console.log ('registering subscription routes with express');
+app.get('/events/:name/subscriptions/', event.eventSubscriptions);
+app.get('/events/:name/subscribe/', event.subscribe);
+app.get('/events/:name/unsubscribe/', event.subscribe);
+ 
 console.log ('registering signal routes with express');
 app.post('/signals', signal.processSignal);
 
