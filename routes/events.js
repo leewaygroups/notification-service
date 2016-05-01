728e0b2b88d8aff68e810bec3be4e180ab71ea24
leewaygroups <leewaygroups@gmail.com>
2016-05-02 05:12:31 +0700

    eventManager

--- routes/events.js
+++ routes/events.js
@@ -1,47 +1,26 @@
-var mongo = require('mongodb');
-var BSON = mongo.BSONPure;
-
-var dbutils = require('../lib/dbutils');
-var mongoUri = dbutils.mongoUri;
+var eventManager = require('../lib/db');
 var eventTypes = require('../lib/eventTypes');
 
-/*
-Events look like this in JSON
+/** ****************************************
+ * All routes return a promise.
+ * 
+{
+"_id": "353dfg34334bbnm34545345",
+"eventName": "CUSTOMER.SERVICE.New_Tecket",
+"eventType": "EventTypes.SYSTEM_NOTIFICATION"
+"eventSource": ['App.CustomerService']
+}
+********************************************/
 
-  {
-    "_id": "353dfg34334bbnm34545345",
-    "eventName": "CUSTOMER.SERVICE.New_Tecket",
-    "eventType": "EventTypes.SYSTEM_NOTIFICATION"
-    "eventSource": ['App.CustomerService']
-  }
-
-*/
-
-exports.addEvent = function(req, res) {
+exports.registerEvent = function(req, res) {
   var event = req.body;
-
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('events', function(er, collection) {
-      collection.insert(event, {safe:true}, function(err, result) {
-        res.send(result[0]);
-        db.close();
-      });
-    });
-  });
+  return eventManager.register(event);
 }
 
 exports.updateEvent = function(req, res) {
   var id = req.params.id;
   var event = req.body;
-
-  mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('events', function(err, collection) {
-      collection.update({'_id':new BSON.ObjectID(id)}, event, {safe:true}, function(err, result) {
-        res.send(event);
-        db.close();
-      });
-    });
-  });
+  return eventManager.update(event, id);
 }
  
 exports.deleteEvent = function(req, res) {
